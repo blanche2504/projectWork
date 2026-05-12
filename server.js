f3539606de8696d6d6776cf96090bae1540d1f32
Blanche <65652563+Blanche25@users.noreply.github.com>
2026-05-12 11:58:46 +0200

    readme

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { MongoClient } from "mongodb";
+import cors from "cors";
 
 const PORT = 3000;
 const APP = express();
@@ -7,35 +8,36 @@ const MONGO_URI = "mongodb://localhost:27017";
 const DB_NAME = "pw_libreria";
 
 APP.use(express.json());
+APP.use(cors());
 
 APP.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
 });
 
 APP.get("/api/books", async (_req, res) => {
-  try {
-    const client = new MongoClient(MONGO_URI);
-    await client.connect();
-    const db = client.db(DB_NAME);
-    const books = await db.collection("books").find({}).toArray();
-    await client.close();
-    res.json(books);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+    try {
+        const client = new MongoClient(MONGO_URI);
+        await client.connect();
+        const db = client.db(DB_NAME);
+        const books = await db.collection("books").find({}).toArray();
+        await client.close();
+        res.json(books);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 async function startServer() {
-  try {
-    APP.listen(PORT, () => {
-      console.log("server running");
-    });
-  } catch (error) {
-    console.error(":(", error);
-    process.exit(1);
-  }
+    try {
+        APP.listen(PORT, () => {
+            console.log("server running");
+        });
+    } catch (error) {
+        console.error(":(", error);
+        process.exit(1);
+    }
 }
 
 startServer();
