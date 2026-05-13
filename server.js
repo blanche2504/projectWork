006ab1ac3f78e745897f7d937cbcc68fc4072a8b
Blanche <65652563+Blanche25@users.noreply.github.com>
2026-05-13 14:24:06 +0200

    header, cleanup generale

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ APP.get("/api/books", async (_req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-  import { MongoClient } from "mongodb";
 });
 
 // Aggiunge libro //
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import BookList from "./components/BookList.jsx";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import AddStar from "./components/AddBook.jsx";
 import AddBook from "./components/AddBook.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
-  return (
-    <div className="container mt-4">
-      <h1>Biblioteca Piazzalunga</h1>
-      <BookList />
-      <AddBook />
-    </div>
-  );
+    return (
+        <div className="container mt-4">
+            <Header />
+            <BookList />
+            <AddBook />
+        </div>
+    );
 }
 
 export default App;
