42e4f85ab59c37ff363dbd41341d36a8471fa55d
Blanche <65652563+Blanche25@users.noreply.github.com>
2026-05-19 10:40:05 +0200

    banner img

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,49 @@ import AddBook from "./components/AddBook.jsx";
 import Header from "./components/Header.jsx";
 import Login from "./components/Login.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from "./components/Banner.jsx";
 
 function App() {
-  const [showLogin, setShowLogin] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    !!localStorage.getItem("token"),
-  );
+    const [showLogin, setShowLogin] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(
+        !!localStorage.getItem("token"),
+    );
 
-  function handleLogin() {
-    setIsAuthenticated(true);
-  }
+    function handleLogin() {
+        setIsAuthenticated(true);
+    }
 
-  function handleLogout() {
-    localStorage.removeItem("token");
-    setIsAuthenticated(false);
-  }
+    function handleLogout() {
+        localStorage.removeItem("token");
+        setIsAuthenticated(false);
+    }
 
-  return (
-    <div className="container mt-4">
-      <Header
-        isAuthenticated={isAuthenticated}
-        onLogout={handleLogout}
-        onShowLogin={() => setShowLogin(true)}
-      />
-      <BookList isAuthenticated={isAuthenticated} />
-      <hr />
-      {isAuthenticated ? (
-        <AddBook />
-      ) : (
-        <p className="text-muted text-center">
-          Effettua il login per aggiungere, modificare o eliminare libri.
-        </p>
-      )}
-      {showLogin && <Login onLogin={handleLogin} onClose={() => setShowLogin(false)} />}
-    </div>
-  );
+    return (
+        <div className="container mt-4 noTopMargin">
+            <Banner />
+            <Header
+                isAuthenticated={isAuthenticated}
+                onLogout={handleLogout}
+                onShowLogin={() => setShowLogin(true)}
+            />
+            <BookList isAuthenticated={isAuthenticated} />
+            <hr />
+            {isAuthenticated ? (
+                <AddBook />
+            ) : (
+                <p className="text-muted text-center">
+                    Effettua il login per aggiungere, modificare o eliminare
+                    libri.
+                </p>
+            )}
+            {showLogin && (
+                <Login
+                    onLogin={handleLogin}
+                    onClose={() => setShowLogin(false)}
+                />
+            )}
+        </div>
+    );
 }
 
 export default App;
